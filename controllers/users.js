2df44d5de12489b0fbaaccdc37a6ce5179e7b652
tamij12 <tamijangosrz@gmail.com>
2023-10-17 00:36:45 -0600

    controllers/users-post

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 //const fs = require('fs')
-//const Users = './db/users.json'
-// const Users = require('../models/users')
-// const auth = require('../middlewares/auth')
+// const Users = './db/users.json'
+ const Users = require('../models/users')
+ const auth = require('../middlewares/auth')
 
 //db.users.find({})
 
@@ -46,25 +46,4 @@ module.exports = {
         let token = auth.generateToken(user)
         return res.status(200).send({ msg: "success", data: token })
     }
-    /*put: (req, res) => {
-        let id = req.params.id
-
-        fs.readFile(Users, 'utf8', (err, usersData) => {
-            if (err) res.status(404).send({msg: 'users db not found', error: err});
-            let users = JSON.parse(usersData)
-            //let userById = users.find((u) => u.id == id)
-            let newUsers = []
-            users.forEach(user => {
-                if (user.id != id) {
-                    newUsers.push(user)
-                } else {
-                    newUsers.push(req.body)
-                }               
-            });
-            fs.writeFile(Users, JSON.stringify(newUsers), (err) => {
-                if (err) res.status(403).send({msg: 'user not updated', error: err});
-                res.status(200).send({msg: 'user updated', data: req.body});              
-            });
-        });   
-    },*/
 }
