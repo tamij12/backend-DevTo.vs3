3af94b3251b1234939cb2e60232c29353a6a129b
curcomil <sadielsalgado98@gmail.com>
2023-10-18 14:00:39 -0600

    rutas y validación update

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post");
+const authMiddleware = require("../middlewares/auth");
 
 router.post("/", postController.createPost);
 router.get("/:id", postController.getPostById);
-router.put("/:id", postController.updatePost);
+router.put("/:id", [authMiddleware.authToken, postController.updatePost]);
 
 module.exports = router;
