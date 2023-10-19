804ad77a5f58b159e04a83b7be3e2c2f40441520
tamij12 <tamijangosrz@gmail.com>
2023-10-18 18:38:11 -0600

    changes delete-post

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post");
+const authMiddleware = require("../middlewares/auth");
 
-router.post("/", postController.createPost);
+router.post("/new_post/", postController.createPost);
 router.get("/:id", postController.getPostById);
 router.put("/:id", postController.updatePost);
-// router.delete("/:id", postController.deletePost);
+router.delete("/:id", postController.deletePost);
 
 module.exports = router;
